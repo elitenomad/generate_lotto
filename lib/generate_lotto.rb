bdf73c1a2bf15cb450439cef1d60554a536ae602
elitenomad <stalin.pranava@gmail.com>
2015-06-25 21:14:24 +1000

    Fix generate_lotto railtie require

--- lib/generate_lotto.rb
+++ lib/generate_lotto.rb
@@ -3,6 +3,7 @@ require "generate_lotto/drawing"
 require "generate_lotto/core_ext"
 require "generate_lotto/configuration"
 require "generate_lotto/helper"
+require "generate_lotto/railstie" if defined?(Rails)
 
 begin
     require "pry"
